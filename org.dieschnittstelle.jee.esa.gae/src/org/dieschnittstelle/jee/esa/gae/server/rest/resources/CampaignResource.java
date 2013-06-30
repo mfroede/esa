3d708dbe0c0168fdec4dd5a6fb462777d30233a7
Frank Schmidt <fschmidt2011@googlemail.com>
2013-06-30 16:51:16 +0200

    blah

--- org.dieschnittstelle.jee.esa.gae/src/org/dieschnittstelle/jee/esa/gae/server/rest/resources/CampaignResource.java
+++ org.dieschnittstelle.jee.esa.gae/src/org/dieschnittstelle/jee/esa/gae/server/rest/resources/CampaignResource.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.logging.Logger;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -31,6 +32,13 @@ public class CampaignResource {
    }
 
    @PUT
+   @Consumes(MediaType.APPLICATION_JSON)
+   @Produces(MediaType.APPLICATION_JSON)
+   public Campaign createCampaign(JAXBElement<Campaign> campaignDTO) {
+      return campaignDTO.getValue();
+   }
+
+   @GET
    @Consumes(MediaType.APPLICATION_JSON)
    @Produces(MediaType.APPLICATION_JSON)
    public Campaign createCampaign(JAXBElement<Campaign> campaignDTO) {
