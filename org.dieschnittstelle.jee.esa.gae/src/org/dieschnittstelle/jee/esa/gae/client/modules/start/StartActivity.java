b7946e59b9b060dda5e44bb940121ca1ff8e85ad
schaeferC <claudia.schaef@gmx.net>
2013-09-21 20:40:07 +0200

    campaign rest service anbindung

--- org.dieschnittstelle.jee.esa.gae/src/org/dieschnittstelle/jee/esa/gae/client/modules/start/StartActivity.java
+++ org.dieschnittstelle.jee.esa.gae/src/org/dieschnittstelle/jee/esa/gae/client/modules/start/StartActivity.java
@@ -1,9 +1,10 @@
 package org.dieschnittstelle.jee.esa.gae.client.modules.start;
 
+import java.util.List;
+
 import org.dieschnittstelle.jee.esa.gae.client.common.AbstractActivityDefaultImpl;
 import org.dieschnittstelle.jee.esa.gae.client.services.Services;
-import org.dieschnittstelle.jee.esa.gae.shared.entities.dto.CustomerDTO;
-import org.dieschnittstelle.jee.esa.gae.shared.entities.dto.LoginDTO;
+import org.dieschnittstelle.jee.esa.gae.shared.entities.dto.CampaignDTO;
 import org.fusesource.restygwt.client.Method;
 import org.fusesource.restygwt.client.MethodCallback;
 
@@ -11,44 +12,62 @@ import com.google.gwt.user.client.ui.AcceptsOneWidget;
 import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
 
-public class StartActivity extends AbstractActivityDefaultImpl<StartPlace> implements StartView.Presenter {
+public class StartActivity extends AbstractActivityDefaultImpl<StartPlace>
+		implements StartView.Presenter {
 
-   private final EventBus eventBus;
-   private final StartView view;
+	private final EventBus eventBus;
+	private final StartView view;
 
-   @Inject
-   public StartActivity(final StartView view, EventBus eventBus) {
-      this.view = view;
-      this.eventBus = eventBus;
-   }
+	@Inject
+	public StartActivity(final StartView view, EventBus eventBus) {
+		this.view = view;
+		this.eventBus = eventBus;
+	}
 
-   @Override
-   public void start(final AcceptsOneWidget panel, final com.google.gwt.event.shared.EventBus pEventBus) {
-      view.setPresenter(this);
-      panel.setWidget(view);
+	@Override
+	public void start(final AcceptsOneWidget panel,
+			final com.google.gwt.event.shared.EventBus pEventBus) {
+		view.setPresenter(this);
+		panel.setWidget(view);
 
-   }
+	}
 
-   @Override
-   public void onButtonClick() {
-      LoginDTO loginDTO = new LoginDTO();
+	@Override
+	public void onButtonClick() {
+		// LoginDTO loginDTO = new LoginDTO();
+		//
+		// loginDTO.setUserName("mail@nowere.com");
+		// loginDTO.setPassword("password");
+		//
+		// Services.login().login(loginDTO, new MethodCallback<CustomerDTO>() {
+		//
+		// @Override
+		// public void onSuccess(Method method, CustomerDTO response) {
+		// // TODO login success
+		//
+		// }
+		//
+		// @Override
+		// public void onFailure(Method method, Throwable exception) {
+		// // TODO login failed
+		// }
+		// });
 
-      loginDTO.setUserName("mail@nowere.com");
-      loginDTO.setPassword("password");
+		Services.campaigns().getAllCampaigns(
+				new MethodCallback<List<CampaignDTO>>() {
 
-      Services.login().login(loginDTO, new MethodCallback<CustomerDTO>() {
+					@Override
+					public void onFailure(Method method, Throwable exception) {
+						System.out.println("");
 
-         @Override
-         public void onSuccess(Method method, CustomerDTO response) {
-            // TODO login success
+					}
 
-         }
-
-         @Override
-         public void onFailure(Method method, Throwable exception) {
-            // TODO login failed
-         }
-      });
-   }
+					@Override
+					public void onSuccess(Method method,
+							List<CampaignDTO> response) {
+						System.out.println();
+					}
+				});
+	}
 
 }
--- org.dieschnittstelle.jee.esa.gae/src/org/dieschnittstelle/jee/esa/gae/client/services/Services.java
+++ org.dieschnittstelle.jee.esa.gae/src/org/dieschnittstelle/jee/esa/gae/client/services/Services.java
@@ -6,14 +6,25 @@ import org.fusesource.restygwt.client.RestServiceProxy;
 import com.google.gwt.core.client.GWT;
 
 public class Services {
-   private static final String BASE_URL = "http://127.0.0.1:8888/rest";
-   private static LoginResourceAsync loginResourceAsync;
+	private static final String BASE_URL = "http://127.0.0.1:8888/rest";
+	private static LoginResourceAsync loginResourceAsync;
+	private static CampaignResourceAsync campaignResourceAsync;
 
-   public static final LoginResourceAsync login() {
-      if (loginResourceAsync == null) {
-         loginResourceAsync = GWT.create(LoginResourceAsync.class);
-         ((RestServiceProxy) loginResourceAsync).setResource(new Resource(BASE_URL + "/login"));
-      }
-      return loginResourceAsync;
-   }
+	public static final LoginResourceAsync login() {
+		if (loginResourceAsync == null) {
+			loginResourceAsync = GWT.create(LoginResourceAsync.class);
+			((RestServiceProxy) loginResourceAsync).setResource(new Resource(
+					BASE_URL + "/login"));
+		}
+		return loginResourceAsync;
+	}
+
+	public static final CampaignResourceAsync campaigns() {
+		if (campaignResourceAsync == null) {
+			campaignResourceAsync = GWT.create(CampaignResourceAsync.class);
+			((RestServiceProxy) campaignResourceAsync)
+					.setResource(new Resource(BASE_URL + "/campaign"));
+		}
+		return campaignResourceAsync;
+	}
 }
